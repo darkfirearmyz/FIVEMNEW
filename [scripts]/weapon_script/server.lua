ba77343ad1baa33c7dc28502a7dd17781e5b8003
Ziggie <ninjagaming12go@users.noreply.github.com>
2026-04-15 21:02:04 +0100

    bug

--- [scripts]/weapon_script/server.lua
+++ [scripts]/weapon_script/server.lua
@@ -1,6 +1,6 @@
 local QBCore = exports['qb-core']:GetCoreObject()
 
-RegisterNetEvent("npc_weaponshop:server:buyWeapon", function(weapon, price)
+RegisterNetEvent"npc_weaponshop:server:buyWeapon", function(weapon, price)
     local src = source
     local Player = QBCore.Functions.GetPlayer(src)
 
