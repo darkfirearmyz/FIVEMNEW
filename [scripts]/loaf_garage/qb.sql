b0a71aa9d5eb09c86c5ed14cfe69536b6c458674
darkfirearmyz <94003555+darkfirearmyz@users.noreply.github.com>
2026-04-16 19:24:00 +0100

    updated garage and loading screen

--- [scripts]/loaf_garage/qb.sql
+++
@@ -1,5 +0,0 @@
--- if this sql file gives you an error, please update your Maria DB to 10.219 or higher.
-ALTER TABLE `player_vehicles` ADD COLUMN IF NOT EXISTS `damages` LONGTEXT;
-ALTER TABLE `player_vehicles` ADD COLUMN IF NOT EXISTS `garage` VARCHAR(50) NOT NULL DEFAULT "square";
-
-UPDATE `player_vehicles` SET `damages`=NULL WHERE `damages` IS NOT NULL;
