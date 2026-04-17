14b532c3eba55216ac46015e495a0a6852c829bc
Ziggie <ninjagaming12go@users.noreply.github.com>
2026-04-17 17:57:26 +0100

    british bins

---
+++ [Props]/bins/fxmanifest.lua
@@ -0,0 +1,16 @@
+fx_version 'cerulean'
+game 'gta5'
+
+author 'YourName'
+description 'Custom british bins'
+version '1.0.0'
+
+this_is_a_map 'yes'
+
+files {
+    'stream/*.ydr',  -- models (meshes)
+    'stream/*.ytd',  -- textures
+    'stream/*.ytyp'  -- optional: archetype definitions
+}
+
+data_file 'DLC_ITYP_REQUEST' 'stream/*.ytyp'
